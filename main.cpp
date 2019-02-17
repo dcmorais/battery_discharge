f7b3d3c24f616ad2d8840e714d59502df37f8302
Daniel Morais <dcmorais@gmail.com>
2019-02-17 09:39:29 -0300

    Fixing command string and change time calculation

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
+    #include <iostream>
 #include <fstream>
 #include <cstring>
 #include <string>
 #include <ctime>
+#include <ratio>
+#include <chrono>
 
 #ifdef _WIN32
 #include <Windows.h>
@@ -13,16 +15,16 @@
 #include "rs232.h"
 
 void send_rs232(int cport_nr, char  command[512]){
-    printf("-- Send command: %s\n", command);
+    printf("-- Send command: %s", command);
     RS232_cputs(cport_nr, command);
 #ifdef _WIN32
     Sleep(1000);
 #else
-    usleep(1000000);  /* sleep for 1 Second */
+    usleep(100000);  /* sleep for 100 milliSeconds */
 #endif
 };
 
-float read_rs232(int cport_nr, unsigned char *buf, int size) {
+double read_rs232(int cport_nr, unsigned char *buf, int size) {
     int n;
 
     printf("- Reading data from DC Charge\n");
@@ -46,17 +48,19 @@ float read_rs232(int cport_nr, unsigned char *buf, int size) {
 }
 
 int main() {
+    using namespace std;
+    using namespace std::chrono;
+
     // +-------------------
     // | Variables
     // +-------------------
     // RS-232 Configuration
-    int n,
-            cport_nr=7,        /* /dev/ttys0 (com1 on windows) */
-                               /*{"\\\\.\\COM1",  "\\\\.\\COM2",  "\\\\.\\COM3",  "\\\\.\\COM4",
-                                  "\\\\.\\COM5",  "\\\\.\\COM6",  "\\\\.\\COM7",  "\\\\.\\COM8",
-                                  "\\\\.\\COM9",  "\\\\.\\COM10", "\\\\.\\COM11", "\\\\.\\COM12",
-                                  "\\\\.\\COM13", "\\\\.\\COM14", "\\\\.\\COM15", "\\\\.\\COM16"};*/
-            bdrate=9600;       /* 9600 baud */
+    double n;
+    int cport_nr=7,        //{"\\\\.\\COM1",  "\\\\.\\COM2",  "\\\\.\\COM3",  "\\\\.\\COM4",
+                           // "\\\\.\\COM5",  "\\\\.\\COM6",  "\\\\.\\COM7",  "\\\\.\\COM8",
+                           // "\\\\.\\COM9",  "\\\\.\\COM10", "\\\\.\\COM11", "\\\\.\\COM12",
+                           // "\\\\.\\COM13", "\\\\.\\COM14", "\\\\.\\COM15", "\\\\.\\COM16"};
+        bdrate=9600;       // 9600 baud
 
     unsigned char buf[4096];
 
@@ -65,23 +69,24 @@ int main() {
     // Variables to configure the DC charge
     char   command[512];          // Sends the command to the DC charge
     char   aux[512];              // Help to send complex word into the command
-    double Eodv            = 8.4; // End of discharge voltage for single cell
+    double Eodv            = 9; // End of discharge voltage for single cell
     int    Number_of_cells = 3;   // Number of cells to be discharged in series
     double Discharge_at    = .05; // Constant current discharge rate in amperes
-    float  Battery_volt    = 12;  // Battery volt returns from DC charge
-    float  Battery_curr;          // Battery current messured 
+    double Battery_volt    = 11.55;// Battery volt returns from DC charge
+    double Battery_curr;          // Battery current messured
 
     // Timing variables
-    clock_t start, end;
-    double cpu_time;
+    high_resolution_clock::time_point start_clk, end_clk;
+    duration<double>                  cpu_time;
 
     // Writing into a file
-    std::ofstream file;
+    ofstream file;
 
+    //file << Battery_volt << std::endl;
     // +-----------------------
     // | Opening the COM port
     // +-----------------------
-    printf("Opening the COM port\n");    
+    printf("Opening the COM port\n");
     if(RS232_OpenComport(cport_nr, bdrate, mode))
     {
         printf("can not open comport\n");
@@ -89,8 +94,8 @@ int main() {
         return(0);
     }
 
-    printf("Enable RST\n");
-    RS232_enableRTS(cport_nr);
+   // printf("Enable RST\n");
+   // RS232_enableRTS(cport_nr);
 
     // +--------------------------
     // | Configure the DC charge
@@ -98,27 +103,50 @@ int main() {
     printf("Configure the DC charge\n");
     // Selects Chan 1; Disables input
     printf("- Selects Chan 1 and Disables input\n");
-    strcpy(command, "CHAN 1;:INPUT OFF");
+    strcpy(command, "CHAN 1;:INPUT OFF\n");
     send_rs232(cport_nr, command);
+    memset(command, 0, sizeof(command)); //clean command
 
     // Sets CC mode
     printf("- Sets CC mode\n");
-    strcpy(command, "FUNCTION CURRENT");
+    strcpy(command, "FUNCTION CURRENT\n");
     send_rs232(cport_nr, command);
+    memset(command, 0, sizeof(command)); //clean command
 
     // Sets the CC level
     printf("- Sets the curr level\n");
     printf("-- Type the current: ");
     std::cin >> Discharge_at;
-    sprintf(aux, "CURRENT:LEVEL %f", Discharge_at);
+    sprintf(aux, "CURRENT:LEVEL %f\n", Discharge_at);
     strcpy(command, aux);
     send_rs232(cport_nr, command);
+    memset(command, 0, sizeof(command)); //clean command
+
+/*
+    printf("- Measure the battery voltage\n");
+    strcpy(command, "MEASURE:VOLTAGE?\n");
+    send_rs232(cport_nr, command);
+    memset(command, 0, sizeof(command)); //clean command
+
+    // Get answer  from DC Charge
+    Battery_volt = read_rs232(cport_nr, buf, 4095);
+    printf("- Total cell voltage: %g\n",  Battery_volt);
+            printf("- Measure the current\n");
+            strcpy(command, "MEASURE:CURRENT?\n");
+            send_rs232(cport_nr, command);
+            memset(command, 0, sizeof(command)); //clean command
+
+            // Get answer  from DC Charge
+            Battery_curr = read_rs232(cport_nr, buf, 4095);
+            printf("- Actual current: %g\n", Battery_curr);
+
+*/
 
     // Enables the input
     printf("- Enables input\n");
-    strcpy(command, "INPUT ON");
+    strcpy(command, "INPUT ON\n");
     send_rs232(cport_nr, command);
-
+    memset(command, 0, sizeof(command)); //clean command
     // +--------------------------
     // | Start Test
     // +--------------------------
@@ -127,40 +155,59 @@ int main() {
     
     // Open file
     sprintf(aux, "save_curr_%f.txt", Discharge_at);
-    file.open(aux, std::ofstream::out);
+    file.open(aux, ofstream::out);
 
     printf("Start test\n");
-    start = clock();
+    start_clk = high_resolution_clock::now();
     while(Battery_volt>Eodv) {
         // Measure the battery voltage
         printf("- Measure the battery voltage\n");
-        strcpy(command, "MEASURE:VOLTAGE?");
+        strcpy(command, "MEASURE:VOLTAGE?\n");
         send_rs232(cport_nr, command);
+        memset(command, 0, sizeof(command)); //clean command
 
         // Get answer  from DC Charge
         Battery_volt = read_rs232(cport_nr, buf, 4095);
-        printf("- Total cell voltage: %f\n",  Battery_volt);
-        //Battery_volt = std::stof((char *)buf);
+        //Battery_volt = stof((char *)buf);
 
 
         // Measure the current
         printf("- Measure the current\n");
-        strcpy(command, "MEASURE:CURRENT?");
+        strcpy(command, "MEASURE:CURRENT?\n");
         send_rs232(cport_nr, command);
+        memset(command, 0, sizeof(command)); //clean command
 
         // Get answer  from DC Charge
         Battery_curr = read_rs232(cport_nr, buf, 4095);
+
+        //Print
+        printf("- Total cell voltage: %f\n",  Battery_volt);
         printf("- Actual current: %f\n", Battery_curr);
-        //Battery_curr = std::stof((char *)buf);
+        //Battery_curr = stof((char *)buf);
 
         // Computed spend time
-        end = clock();
-        cpu_time = ((double) (end-start)) / CLOCKS_PER_SEC;
+        end_clk = high_resolution_clock::now();
+        cpu_time = duration_cast<duration<double>>(end_clk-start_clk);
         printf("- Used time: %f\n", cpu_time);
 
-        // Save received data into a file
-        file << Battery_curr << " " << cpu_time << std::endl;
+        // Save received data into a fill
+        memset(aux, 0, sizeof(aux));
+        sprintf(aux, "%.5g %.5g %.8g\n",Battery_volt, Battery_curr, cpu_time);
+        file.write(aux, 512);
+#ifdef _WIN32
+        Sleep(500);
+#else
+//      usleep(100000);  /* sleep for 100 milliSeconds */
+        usleep(500000);  /* sleep for 500 milliSeconds */
+#endif
     }
 
+    printf("- Disables input\n");
+    strcpy(command, ":INPUT OFF\n");
+    send_rs232(cport_nr, command);
+    memset(command, 0, sizeof(command)); //clean command
+
     file.close();
+
+    return(0);
 }
