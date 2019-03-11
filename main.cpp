ae6737b5eb772e23667cb9cf78cbe43f08338b92
Daniel Morais <dcmorais@gmail.com>
2019-03-11 16:24:16 -0300

    Added select menu

--- main.cpp
+++ main.cpp
@@ -1,28 +1,16 @@
 #include <iostream>
-#include <fstream>
-#include <cstring>
-#include <string>
-#include <ctime>
-#include <ratio>
-#include <chrono>
 
-#ifdef _WIN32
-#include <Windows.h>
-#else
-#include <unistd.h>
-#endif
-
-#include "electronic_load.h"
+#include "electronic_load_tests.h"
 
 int main() {
+
     using namespace std;
-    using namespace std::chrono;
 
     // +-------------------
     // | Variables
     // +-------------------
     // RS-232 Configuration
-    double n;
+    int n;
     int cport_nr=7,        //{"\\\\.\\COM1",  "\\\\.\\COM2",  "\\\\.\\COM3",  "\\\\.\\COM4",
                            // "\\\\.\\COM5",  "\\\\.\\COM6",  "\\\\.\\COM7",  "\\\\.\\COM8",
                            // "\\\\.\\COM9",  "\\\\.\\COM10", "\\\\.\\COM11", "\\\\.\\COM12",
@@ -33,112 +21,40 @@ int main() {
 
     char mode[]={'8','n','1',0}; //{data, parity, stop, }
 
-    // Variables to configure the DC charge
-    char   command[512];          // Sends the command to the DC charge
-    char   aux[512];              // Help to send complex word into the command
-    double Eodv            = 10.5;// End of discharge voltage for single cell
-    int    Number_of_cells = 3;   // Number of cells to be discharged in series
-    double Discharge_at    = .05; // Constant current discharge rate in amperes
-    double Battery_volt    = 11.55;// Battery volt returns from DC charge
-    double Battery_curr;          // Battery current messured
+    bool menu = true;
+    int choice;
 
-    // Timing variables
-    high_resolution_clock::time_point start_clk, end_clk;
-    duration<double>                  cpu_time;
+    while(menu){
+        printf("Select what you want to do:\n");
+        printf("1 - Connection test\n");
+        printf("2 - Battery Discharge test\n");
+        printf("9 - Exit\n");
+        printf(">> ");
+        cin >> choice;
 
+        switch(choice){
+            case 1:
+                n = check_connection(cport_nr, bdrate, mode);
+                menu = false;
+                break;
+            case 2:
+                n = battery_discharge(cport_nr, bdrate, mode);
+                menu = false;
+                break;
+            case 9:
+                menu = false;
+                break;
+            default:
+                printf("Invalid option\n");
+                break;
+        }
 
-    //Define Eletronic Load
-    electronic_load electronic_load(cport_nr);
-
-    // Writing into a file
-    ofstream file;
-
-    //file << Battery_volt << std::endl;
-    // +-----------------------
-    // | Opening the COM port
-    // +-----------------------
-    printf("Opening the COM port\n");
-    if(open_comport(cport_nr, bdrate, mode))
-    {
-        printf("can not open comport\n");
-
-        return(0);
+        if(n == 1){
+            printf("ATTENTION: check your RS232 port!\n");
+            return 0;
+        }
     }
 
-   // printf("Enable RST\n");
-   // RS232_enableRTS(cport_nr);
-
-    // +--------------------------
-    // | Configure the DC charge
-    // +--------------------------
-    printf("Configure the DC charge\n");
-    // Selects Chan 1; Disables input
-    printf("- Selects Chan 1 and Disables input\n");
-    electronic_load.select_chan(1);
-    electronic_load.disable_inputs();
-
-    // Sets CC mode
-    printf("- Sets CC mode\n");
-    electronic_load.set_cc_mode();
-
-    // Sets the CC level
-    printf("- Sets the curr level\n");
-    printf("-- Type the current: ");
-    std::cin >> Discharge_at;
-    electronic_load.set_curr_level(Discharge_at);
-
-    // Enables the input
-    printf("- Enables input\n");
-    electronic_load.enable_inputs();
-
-    // +--------------------------
-    // | Start Test
-    // +--------------------------
-    // Starts test routine that continuously measures and reads
-    // back the voltage and current until batteries are completely discharged
-    
-    // Open file
-    sprintf(aux, "save_curr_%f.txt", Discharge_at);
-    file.open(aux, ofstream::out);
-
-    printf("Start test\n");
-    start_clk = high_resolution_clock::now();
-    while(Battery_volt>Eodv) {
-        // Measure the battery voltage
-        printf("- Measure the battery voltage\n");
-        Battery_volt = electronic_load.measure_battery_volt();
-
-        // Measure the current
-        printf("- Measure the current\n");
-        strcpy(command, "MEASURE:CURRENT?\n");
-        Battery_curr = electronic_load.measure_battery_curr();
-
-        //Print
-        printf("- Total cell voltage: %f\n",  Battery_volt);
-        printf("- Actual current: %f\n", Battery_curr);
-        //Battery_curr = stof((char *)buf);
-
-        // Computed spend time
-        end_clk = high_resolution_clock::now();
-        cpu_time = duration_cast<duration<double>>(end_clk-start_clk);
-        printf("- Used time: %f\n", cpu_time);
-
-        // Save received data into a fill
-        memset(aux, 0, sizeof(aux));
-        sprintf(aux, "%.5g %.5g %.8g\n",Battery_volt, Battery_curr, cpu_time);
-        file.write(aux, 512);
-#ifdef _WIN32
-        Sleep(500);
-#else
-//      usleep(100000);  /* sleep for 100 milliSeconds */
-        usleep(500000);  /* sleep for 500 milliSeconds */
-#endif
-    }
-
-    printf("- Disables input\n");
-    electronic_load.disable_inputs();
-
-    file.close();
 
     return(0);
 }
